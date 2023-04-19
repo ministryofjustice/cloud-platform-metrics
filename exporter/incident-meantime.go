728e7b3d8bd09408680767fa7b2418d1e0cc8928
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-04-19 12:18:26 +0100

    Add incident-meantime tests, split functions to make test easy

--- exporter/incident-meantime.go
+++ exporter/incident-meantime.go
@@ -21,11 +21,7 @@ const (
 	escapePath = "runbooks/source/incident-log.html.md.erb"
 )
 
-func FetchIncidentMTTR() ([]map[string]float64, error) {
-
-	mttrReport := make([]map[string]float64, 0)
-
-	mttrMap := make(map[string]float64)
+func FetchIncidentMTTR() (mttrReport []map[string]float64, error error) {
 
 	token := os.Getenv("GITHUB_OAUTH_TOKEN")
 	// Authenticate to github using auth token
@@ -36,73 +32,81 @@ func FetchIncidentMTTR() ([]map[string]float64, error) {
 	opt := &github.RepositoryContentGetOptions{Ref: "main"}
 	fileContent, _, _, err := client.Repositories.GetContents(context.Background(), owner, repo, escapePath, opt)
 	if err != nil {
-		fmt.Println(err)
-		return nil, err
+		return nil, fmt.Errorf("failed to GetContents from the github repo: %w", err)
 	}
 	if fileContent != nil {
 		// TODO Read the fileContent from github.RepositoryContent
 		content, err := fileContent.GetContent()
 		if err != nil {
-			fmt.Println(err)
-			return nil, err
+			return nil, fmt.Errorf("failed to the raw GetContent from the github repo: %w", err)
 		}
 		// Read the file line by line
-		scanner := bufio.NewScanner(strings.NewReader(content))
-		scanner.Split(bufio.ScanLines)
-		var lines []string
-		for scanner.Scan() {
-			lines = append(lines, scanner.Text())
+		lines := scanLines(content)
+		mttrReport, err = parseMTTRQuarter(lines)
+	}
+	return mttrReport, nil
+}
+
+// scanLines reads a file into a slice of lines
+func scanLines(content string) (lines []string) {
+	// Read the file line by line
+	scanner := bufio.NewScanner(strings.NewReader(content))
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		// Ignore empty lines
+		if scanner.Text() == "" {
+			continue
 		}
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
 
-		// Loop through the lines
-		for i, line := range lines {
-			match, err := regexp.MatchString("^## Q[1-4]", line)
-			if err != nil {
-				fmt.Println(err)
-				return nil, err
-			}
-			if match {
-				// parse the quarter value from the line
-				quarter := line[3:10]
-				// get the current quarter
-				current_quarter := mogo.FormatQuarter(time.Now())
-				if quarter == current_quarter {
-					// goto the next line
-					mttr := string(lines[i+2])
-					// Parse the MTTRepair value from the line
-					mttr = mttr[27:]
-					// take the single space between duration
-					mttr = strings.ReplaceAll(mttr, " ", "")
-					mttr_time, err := time.ParseDuration(mttr)
-					if err != nil {
-						fmt.Println(err)
-						return nil, err
-					}
-					mttr_hours := mttr_time.Hours()
-					mttr_minutes := mttr_time.Minutes()
-					total_minutes := mttr_hours*60 + mttr_minutes
-					fmt.Println("mttrepair", total_minutes)
-
-					mttrMap["incidents_mean_time_to_repair"] = total_minutes
-					// goto the next line
-					mttr = string(lines[i+4])
-					// Parse the MTTResolve value from the line
-					mttr = mttr[27:]
-					// take the single space between duration
-					mttr = strings.ReplaceAll(mttr, " ", "")
-					mttr_time, err = time.ParseDuration(mttr)
-					if err != nil {
-						fmt.Println(err)
-						return nil, err
-					}
-					mttr_hours = mttr_time.Hours()
-					mttr_minutes = mttr_time.Minutes()
-					total_minutes = mttr_hours*60 + mttr_minutes
-					fmt.Println("mttresolve", total_minutes)
-					mttrMap["incidents_mean_time_to_repair"] = total_minutes
-					mttrReport = append(mttrReport, mttrMap)
-
+// parseMTTRQuarter parses the incident log file and returns the MTTR for the current quarter
+func parseMTTRQuarter(lines []string) (mttrReport []map[string]float64, err error) {
+	mttrMap := make(map[string]float64)
+	// Loop through the lines and match the quarter
+	for i, line := range lines {
+		match, err := regexp.MatchString("^## Q[1-4]", line)
+		if err != nil {
+			return nil, fmt.Errorf("failed to the match reexp Q1-4: %w", err)
+		}
+		if match {
+			// parse the quarter value from the line
+			quarter := line[3:10]
+			// get the current quarter
+			current_quarter := mogo.FormatQuarter(time.Now())
+			if quarter == current_quarter {
+				// goto the next line
+				mttr := string(lines[i+1])
+				// Parse the MTTRepair value from the line
+				mttr = mttr[27:]
+				// take the single space between duration
+				mttr = strings.ReplaceAll(mttr, " ", "")
+				mttr_time, err := time.ParseDuration(mttr)
+				if err != nil {
+					return nil, fmt.Errorf("failed to the MTT Repair from incident log: %w", err)
 				}
+				mttr_hours := mttr_time.Hours()
+				mttr_minutes := mttr_time.Minutes()
+				total_minutes := mttr_hours*60 + mttr_minutes
+				mttrMap["incidents_mean_time_to_repair"] = total_minutes
+				// goto the next line
+				mttr = string(lines[i+2])
+				// Parse the MTTResolve value from the line
+				mttr = mttr[27:]
+				// take the single space between duration
+				mttr = strings.ReplaceAll(mttr, " ", "")
+				mttr_time, err = time.ParseDuration(mttr)
+				if err != nil {
+					return nil, fmt.Errorf("failed to the MTT Resolve from incident log: %w", err)
+				}
+				mttr_hours = mttr_time.Hours()
+				mttr_minutes = mttr_time.Minutes()
+				total_minutes = mttr_hours*60 + mttr_minutes
+				mttrMap["incidents_mean_time_to_resolve"] = total_minutes
+				mttrReport = append(mttrReport, mttrMap)
+
 			}
 		}
 	}
---
+++ exporter/incident-meantime_test.go
@@ -0,0 +1,91 @@
+package exporter
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_scanLines(t *testing.T) {
+	type args struct {
+		content string
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantLines []string
+	}{
+		{
+			name: "Test scanLines",
+			args: args{
+				content: `# Incident Log
+
+> Use the [mean-time-to-repair.rb] script to view performance metrics
+
+## Q2 2023 (January-March)
+
+- **Mean Time to Repair**: 225h 11m
+
+- **Mean Time to Resolve**: 225h 28m`,
+			},
+			wantLines: []string{
+				"# Incident Log",
+				"> Use the [mean-time-to-repair.rb] script to view performance metrics",
+				"## Q2 2023 (January-March)",
+				"- **Mean Time to Repair**: 225h 11m",
+				"- **Mean Time to Resolve**: 225h 28m",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotLines := scanLines(tt.args.content); !reflect.DeepEqual(gotLines, tt.wantLines) {
+				t.Errorf("scanLines() = %v, want %v", gotLines, tt.wantLines)
+			}
+		})
+	}
+}
+
+func Test_parseMTTRQuarter(t *testing.T) {
+	type args struct {
+		lines []string
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantMttrReport []map[string]float64
+		wantErr        bool
+	}{
+		{
+			name: "Test parseMTTRQuarter",
+			args: args{
+				lines: []string{
+					"# Incident Log",
+					"> Use the [mean-time-to-repair.rb] script to view performance metrics",
+					"## Q2 2023 (January-March)",
+					"- **Mean Time to Repair**: 225h 11m",
+					"- **Mean Time to Resolve**: 225h 28m",
+				},
+			},
+			wantMttrReport: []map[string]float64{
+				{
+					"incidents_mean_time_to_repair":  27022,
+					"incidents_mean_time_to_resolve": 27056,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotMttrReport, err := parseMTTRQuarter(tt.args.lines)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseMTTRQuarter() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotMttrReport, tt.wantMttrReport) {
+				t.Errorf("parseMTTRQuarter() = %v, want %v", gotMttrReport, tt.wantMttrReport)
+			}
+		})
+	}
+}
